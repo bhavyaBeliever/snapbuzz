c172d049e7c07a9745c72a7429e353207fe1b439
bhavyaBeliever <bhavyashah8443@gmail.com>
2024-06-16 18:44:03 +0530

    feat: Designed controller to share the userInfo

---
+++ controllers/accessUsersInfoController.js
@@ -0,0 +1,13 @@
+const User=require("../models/userSchema");
+const accessUsersInfoController = (req, res) => {
+    User.find({}, 'username followers following bio')
+      .then((users) => {
+        
+        res.status(200).json(users);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+}
+ 
+module.exports = accessUsersInfoController;
