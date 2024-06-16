3e25384255748941a87b439542567d7046636a18
bhavyaBeliever <bhavyashah8443@gmail.com>
2024-06-16 18:59:28 +0530

    feat: Added Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import SideNav from "./pages/SideNav";
+import Home from "./pages/Home";
+
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useAuthContext } from "./hooks/useAuthContext";
@@ -12,16 +14,20 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={user ? <SideNav /> : <Navigate to="/login" />}
+            exact
+            element={user ? (<><SideNav /> <Home /></>) : <Navigate to="/login" />}
           />
           <Route
             path="/login"
+            exact
             element={!user ? <Login /> : <Navigate to="/" />}
           />
           <Route
             path="/signup"
+            exact
             element={!user ? <SignUp /> : <Navigate to="/" />}
           />
+          
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import style from "../styles/Home.module.css"
+const Home = () => {
+
+  return (
+    <div className={style.home}>
+      <div className={style.container}>
+        <h1>Home</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam quibusdam facilis incidunt, sapiente minima obcaecati nisi, dolores velit temporibus saepe impedit rerum cupiditate ex. Cum illo eum soluta temporibus aliquam similique nemo dolorem exercitationem.</p>
+
+      </div>
+    </div>
+  );
+}
+
+export default Home;
